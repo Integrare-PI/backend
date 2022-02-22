5000cd0f051be4f111115c6d95b708e76e65d888
acceberc <rebecca-cunha@hotmail.com>
2022-02-22 12:44:34 -0300

    task3

---
+++ backend/src/main/java/com/generation/backend/model/Tema.java
@@ -0,0 +1,44 @@
+package com.generation.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "A disciplina é obrigatória!")
+	private String disciplinas;
+	@NotBlank(message = "A descrição é obrigatória.")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(String disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
