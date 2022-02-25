9f7a9762564b7f568a1c694536551b2736904225
Joyce Lopes <joylopes01021@gmail.com>
2022-02-25 11:13:15 -0300

    criação do TemaController

---
+++ backend/src/main/java/com/generation/backend/controller/TemaController.java
@@ -0,0 +1,43 @@
+package com.generation.backend.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.backend.model.Tema;
+import com.generation.backend.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/temas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TemaController {
+
+	@Autowired
+	private TemaRepository temaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll() {
+		return ResponseEntity.ok(temaRepository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable Long id) {
+		return temaRepository.findById(id)
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/tema/{tema}")
+	public ResponseEntity<List<Tema>> getByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+}	
