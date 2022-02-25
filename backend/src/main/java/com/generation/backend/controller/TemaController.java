36f4af3b9b17a692e67a96e866e3b06a0f5e332b
WillMenezesSH <wma_1999@hotmail.comcd Portugol/ clearcleargit config --global user.email wma_1999@hotmail.com>
2022-02-25 11:37:27 -0300

    finalização crud

--- backend/src/main/java/com/generation/backend/controller/TemaController.java
+++ backend/src/main/java/com/generation/backend/controller/TemaController.java
@@ -3,11 +3,18 @@ package com.generation.backend.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,4 +47,31 @@ public class TemaController {
 		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
 	
+	@PostMapping
+	public ResponseEntity <Tema> postTema(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
+		
+	}
+	
+	@PutMapping
+    public ResponseEntity<Tema> putTema (@Valid @RequestBody Tema tema){
+
+        return temaRepository.findById(tema.getId())
+            .map(resposta -> ResponseEntity.ok().body(temaRepository.save(tema)))
+            .orElse(ResponseEntity.notFound().build());
+	
+	}	
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTema(@PathVariable Long id) {
+
+        return temaRepository.findById(id)
+                .map(resposta -> {
+                    temaRepository.deleteById(id);
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+	
+	
+	}
 }	
