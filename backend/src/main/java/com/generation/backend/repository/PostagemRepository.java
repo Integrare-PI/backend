e5c5a39e4f56e4d164241b092ee45639171476d2
Lucas <lucas.orodrigues@hotmail.com>
2022-03-03 11:28:22 -0300

    RepositoryPostagem

---
+++ backend/src/main/java/com/generation/backend/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package com.generation.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.backend.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+		public List<Postagem> findAllByAssuntoContainingIgnoreCase(String assunto);
+}
